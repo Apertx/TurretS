ffbb0894fdc91966d5dd8f44539435c35bf105ba
Apertx <Dirty0wolf@gmail.com>
2020-08-26 16:07:01 +0300

    en array fin

--- app/src/main/java/apertx/turret/sound/MainActivity.java
+++ app/src/main/java/apertx/turret/sound/MainActivity.java
@@ -22,8 +22,6 @@ public class MainActivity extends Activity {
 	static final int SOUND_PRIORITY = 1;
 	static final int SOUND_RES_ID = 0x7f040000;
 
-	static final byte[] quote_offset = {0,3,14,24,30,40,47,54,59,64,69,76,84,90,93,94,97,102};
-
 	SharedPreferences settings;
 	SoundPool sound;
 	byte sound_done;
@@ -32,165 +30,37 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle b0) {
 		super.onCreate(b0);
 		settings = getSharedPreferences("data", MODE_PRIVATE);
-		CharSequence[] po = {
-			"[Turret fire]",
-			"[Turret fire]",
-			"[Turret fire]"
-		};
-		CharSequence[] p1 = {
-			"Wheee.",
-			"Wheeeeeee.",
-			"I'm afraid of heights.",
-			"Noooo.",
-			"See you soon.",
-			"I'm scared.",
-			"I'm scared.",
-			"Hooray.",
-			"Glorious freedom.",
-			"I'm flying.",
-			"Goodbye."
-		};
-		CharSequence[] p2 = {
-			"Ow.",
-			"Oww.",
-			"Owwww.",
-			"It burns.",
-			"I'm on fire.",
-			"I'm on fire owww.",
-			"Please stop.",
-			"You've made your point.",
-			"OK you win.",
-			"This is not good."
-		};
-		CharSequence[] p3 = {
-			"Ooohhh.",
-			"Owwww.",
-			"Can't...breathe.",
-			"Excuse me you're squishing me.",
-			"Um, hello.",
-			"Help! Being squished."
-		};
-		CharSequence[] p4 = {
-			"Hello?",
-			"Hello.",
-			"Target acquired.",
-			"Whyyyy.",
-			"I did everything you askedddd.",
-			"I don't understandddd.",
-			"I'm fineeee",
-			"Ahhhh.",
-			"Wheee oh noooo.",
-			"Whyyy."
-		};
-		CharSequence[] p5 = {
-			"[Activation sound]",
-			"[Alarm sound]",
-			"[Alert sound]",
-			"[Deploy sound]",
-			"[Death sound]",
-			"[Ping sound]",
-			"[Retract sound]"
-		};
-		CharSequence[] p6 = {
-			"Hi.",
-			"Target acquired.",
-			"Firing.",
-			"Hello friend.",
-			"Gotcha.",
-			"There you are.",
-			"I see you."
-		};
-		CharSequence[] p7 = {
-			"Hellooo.",
-			"Searching.",
-			"Sentry mode activated.",
-			"Is anyone there?",
-			"Could you come over here?"
-		};
-		CharSequence[] p8 = {
-			"Coming through.",
-			"Excuse me.",
-			"Sorry.",
-			"My fault.",
-			"Ohh."
-		};
-		CharSequence[] p9 = {
-			"Hello.",
-			"Deploying.",
-			"Activated.",
-			"There you are.",
-			"Who's there?"
-		};
-		CharSequence[] p10 = {
-			"Ahahaa.",
-			"Critical error.",
-			"Shutting down.",
-			"I don't blame you.",
-			"I don't hate you.",
-			"Whyyyy.",
-			"No hard feelings."
-		};
-		CharSequence[] p11 = {
-			"Hey.",
-			"Hey hey hey.",
-			"Put me down.",
-			"Who are you?",
-			"Hey.",
-			"Please put me down.",
-			"Help.",
-			"Uh oh."
-		};
-		CharSequence[] p12 = {
-			"Goodbay.",
-			"Sleep mode activated.",
-			"Hibernating.",
-			"Goodnight.",
-			"Resting.",
-			"Nap time."
-		};
-		CharSequence[] p13 = {
-			"Are you still there?",
-			"Target lost.",
-			"Searching."
-		};
-		CharSequence[] p14 = {
-			"aiaiaiaiaiai."
-		};
-		CharSequence[] p15 = {
-			"Hey, it's me!",
-			"Don't shoot!",
-			"Stop shooting!"
-		};
-		CharSequence[] p16 = {
-			"Aah.",
-			"Self test error.",
-			"Malfunctioning.",
-			"Aaaah.",
-			"Ow ow ow ow owww."
-		};
-		CharSequence[] p17 = {
-			"But I need to protect the humans."
-		};
-		sound_done = 0;
-		ArrayList<ArrayList<Map<String, CharSequence>>> labelList = new ArrayList<>();
-		ArrayList<Map<String, CharSequence>> headerList = new ArrayList<>();
-		ArrayList<Map<String, CharSequence>> itemList = new ArrayList<>();
-		Map<String, CharSequence> map;
 
-		for (CharSequence label: getResources().getStringArray(R.array.quote_headers)) {
-			map = new HashMap<>();
-			map.put("header", label);
+		sound_done = 0;
+		final byte[] quote_offset = {0,3,14,24,30,40,47,54,59,64,69,76,84,90,93,94,97,102,103};
+		ArrayList<ArrayList<Map<String, String>>> labelList = new ArrayList<>();
+		ArrayList<Map<String, String>> headerList;
+		ArrayList<Map<String, String>> itemList;
+		Map<String, String> map;
+
+		String[] headers = getResources().getStringArray(R.array.quote_headers);
+		headerList = new ArrayList<Map<String, String>>();
+		for (int j = 0; j < 18; j += 1) {
+			map = new HashMap<String, String>();
+			map.put("header", headers[j]);
 			headerList.add(map);
 		}
-		for (CharSequence label: po) {
-			map = new HashMap<>();
-			map.put("label", label);
-			itemList.add(map);
+
+		
+		int i = 0;
+		String[] items = getResources().getStringArray(R.array.quote_items);
+		for (int offset = 1; offset < 19; offset += 1) {
+			itemList = new ArrayList<Map<String, String>>();
+			for (; i < quote_offset[offset]; i += 1) {
+				map = new HashMap<String, String>();
+				map.put("item", items[i]);
+				itemList.add(map);
+			}
+			labelList.add(itemList);
 		}
-		labelList.add(itemList);
 
 		ExpandableListView elv = new ExpandableListView(this);
-		SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(this, headerList, android.R.layout.simple_expandable_list_item_1, new String[] {"header"}, new int[] {android.R.id.text1}, labelList, android.R.layout.simple_list_item_1, new String[] {"label"}, new int[] {android.R.id.text1});
+		SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(this, headerList, android.R.layout.simple_expandable_list_item_1, new String[] {"header"}, new int[] {android.R.id.text1}, labelList, android.R.layout.simple_list_item_1, new String[] {"item"}, new int[] {android.R.id.text1});
 		elv.setAdapter(adapter);
 
 		sound = new SoundPool(SOUND_MAX_STREAMS, AudioManager.STREAM_MUSIC, 0);
@@ -219,7 +89,7 @@ public class MainActivity extends Activity {
 				@Override
 				public void run() {
 					for (byte i = 0; i < 103; i++)
-						sound.load(MainActivity.this, SOUND_RES_ID + i, 0);
+						sound.load(MainActivity.this, SOUND_RES_ID + i, SOUND_PRIORITY);
 				}
 			}).start();
 		sound.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
@@ -227,6 +97,8 @@ public class MainActivity extends Activity {
 				public void onLoadComplete(SoundPool p0, int p1, int p2) {
 					if (p2 == 0)
 						sound_done += 1;
+					else
+						finish();
 				}
 			});
 	}
@@ -261,9 +133,17 @@ public class MainActivity extends Activity {
 			case ID_MENU_STAT:
 				new AlertDialog.Builder(this).
 					setTitle(getString(R.string.stat)).
-					setMessage(new StringBuilder().append("").append(": ").append(clicks).toString()).
+					setMessage(new StringBuilder().append(getString(R.string.stat_message)).append(": ").append(clicks).toString()).
 					setPositiveButton(R.string.ok, null).
 					show();
+				break;
+			case ID_MENU_ABOUT:
+				new AlertDialog.Builder(this).
+					setTitle(getString(R.string.about)).
+					setMessage(getString(R.string.about_message)).
+					setPositiveButton(R.string.ok, null).
+					show();
+				break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
